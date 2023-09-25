cbfe08321c5ac4174cfe9c48c6b5eef6bc913b46
jinirobot <jinirobots@gmail.com>
2023-09-25 13:13:26 +0900

    스크래치 패키징 코드 추가

---
+++ scratch-app/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  mainWindow.setMenu(null);
+
+  mainWindow.loadFile('./build/index.html');
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+  if(process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
